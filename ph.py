5088a0a89dce4514c58ce8455c8360ccca7a06e8
insan1tyyy <71565965+insan1tyyy@users.noreply.github.com>
2021-07-30 22:59:18 +0300

    Add files via upload

---
+++ ph.py
@@ -0,0 +1,52 @@
+import vk_api
+from requests import post
+import time
+import json
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+
+vk = vk_api.VkApi(token=config['token']).get_api()
+
+name = 'd3adluvv?'
+
+file = {'file1': open('7.jpg', 'rb'),
+'file2': open('6.jpg', 'rb'),
+'file3': open('5.jpg', 'rb'),
+'file4': open('4.jpg', 'rb'),
+'file5': open('3.jpg', 'rb'),
+'file6': open('2.jpg', 'rb'), 
+'file7': open('1.jpg', 'rb'), }
+
+def upload() -> dict:
+    with open('config.json', 'r') as f:
+        config = json.load(f)
+    url:str = vk.photos.getUploadServer(album_id=config['albumId'])['upload_url']
+    r = post(url, files=file).json()
+    return r
+
+def newAlbum() -> int:
+    albumId = vk.photos.createAlbum(title=name)['id']
+    return albumId
+
+r = upload()
+phlist, hash, server, album = r['photos_list'], r['hash'], r['server'], r['aid']
+
+while True:
+    try:
+        vk.photos.save(album_id=album, hash=hash, server=server, photos_list = phlist)
+        print(time.time(), ' Uploaded. Photo count: ', vk.photos.getAll()['count'])
+    except vk_api.exceptions.VkApiError as e:
+        if "This album is full" in e.error['error_msg']:
+            with open('config.json', 'r') as f:
+                config = json.load(f)
+            config['albumId'] = newAlbum()
+            with open('config.json', 'w') as f:
+                json.dump(config, f, indent = 4)
+            r = upload()
+            phlist, hash, server, album = r['photos_list'], r['hash'], r['server'], r['aid']
+        elif 'Unknown error occurred' in e.error['error_msg'] or 'Flood control' in e.error['error_msg']:
+            print('sleeping 300s', e.error['error_msg'])
+            time.sleep(300)
+            continue
+
